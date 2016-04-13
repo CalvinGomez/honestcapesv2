52316ec5d77443df19c45bcebdd415e7ff1f0f4c
Calvin Gomez <calvin.gomez1@gmail.com>
2016-04-12 21:04:28 -0700

    Added course ID Data

--- routes/chat.js
+++ routes/chat.js
@@ -2,14 +2,14 @@
 
 exports.view = function(req, res) {
 
-
-    models.theNews.find({}, function(err, stuff){
+	var courseID = "570dc2b7e4b0cbcd095d62e4"
+    models.theNews.find({"course_id":courseID}, function(err, stuff){
         if (!err){
             // console.log(stuff);
 
             var data = {'newsfeed': stuff};
             //console.log(data);
-            res.render('chat', data);
+            res.render('chat', data, courseID);
         } else { console.log(err);}
     });
 
