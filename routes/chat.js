ff3223adf257a3fea62cbadab993736329cd84d2
Arnold Chen <arc013@ucsd.edu>
2016-04-13 21:07:24 -0700

    added rating retrieve and push to database code

--- routes/chat.js
+++ routes/chat.js
@@ -1,16 +1,26 @@
  var models = require("../models");
 
 exports.view = function(req, res) {
+    var goodStuff;
 
 	var courseID = "570dc2b7e4b0cbcd095d62e4";
     models.theNews.find({"course_id":courseID}, function(err, stuff){
         if (!err){
              //console.log(courseID);
 
-            var data = {'newsfeed': stuff, 'courseID': courseID};
+            goodStuff=stuff;
+
             //console.log(data);
-            res.render('chat', data);
+
         } else { console.log(err);}
+    });
+    models.OverallRating.findOne({'course_id': courseID}, function(err, rating){
+        if (err) {return err;}
+        var theRating=rating.rating;
+        rating.save(function(err, rating) {
+        });
+        var data = {'newsfeed': goodStuff, 'courseID': courseID, 'rating': theRating};
+        res.render('chat', data);
     });
 
 
