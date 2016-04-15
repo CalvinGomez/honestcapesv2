8f2105823a81eb6fb314593bab87d45cd5a713bf
Arnold Chen <arc013@ucsd.edu>
2016-04-14 21:20:33 -0700

    fixed null rating

--- routes/chat.js
+++ routes/chat.js
@@ -4,15 +4,25 @@ exports.view = function(req, res) {
 
 	var courseID = "570dc2b7e4b0cbcd095d62e4";
     console.log(req.body.courseid);
-    models.theNews.find({"course_id":courseID}, function(err, stuff){
+    models.theNews.find({"course_id":req.body.courseid}, function(err, stuff){
         if (!err){
              //console.log(courseID);
-            models.OverallRating.findOne({'course_id': courseID}, function(err, rating){
+            models.OverallRating.findOne({'course_id': req.body.courseid}, function(err, rating){
                 if (err) { return err; }
-                var theRating=rating.rating;
-                //rating.save(function(err, rating) {
-                //});
-                var data = {'newsfeed': stuff, 'courseID': courseID, 'rating': theRating};
+                if (rating==null){
+                    var newRating= new models.OverallRating({
+                        "course_id": req.body.courseid,
+                        "rating": 0,
+                        "userCount": 0
+                    });
+                    newRating.save(function(err, newRating){if (err) throw err;});
+                } else {
+                    var theRating = rating.rating;
+                    //rating.save(function(err, rating) {
+                    //});
+
+                }
+                var data = {'newsfeed': stuff, 'courseID': req.body.courseid, 'rating': theRating};
                 console.log(data);
                 res.render('chat', data);
 
